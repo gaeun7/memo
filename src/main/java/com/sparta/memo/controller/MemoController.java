12fbf23dbad2373b2507f2cf7b8f7ac56ad5816a
Gaeun <rkdms1111111@naver.com>
2024-05-13 20:31:57 +0900

    Update, Delete 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -11,19 +11,19 @@ import java.util.*;
 @RequestMapping("/api")
 public class MemoController {
 
-    private final Map<Long, Memo> momeList = new HashMap<>();
+    private final Map<Long, Memo> memoList = new HashMap<>();
 
     @PostMapping("/memos")
     public MemoResponseDto creatMemo(@RequestBody MemoRequestDto requestDto) {
-       //   RequestDto -> Entity
+        //   RequestDto -> Entity
         Memo memo = new Memo(requestDto);
 
         // Memo Max ID Check
-        Long maxId = momeList.size() > 0 ? Collections.max(momeList.keySet()) + 1 : 1;
+        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
         memo.setId(maxId);
 
         // DB 저장
-        momeList.put(memo.getId(), memo);
+        memoList.put(memo.getId(), memo);
 
         // Enitiy -> ResponseDto
         MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
@@ -34,9 +34,36 @@ public class MemoController {
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemo() {
         // Map To List
-        List<MemoResponseDto> responseList = momeList.values().stream()
+        List<MemoResponseDto> responseList = memoList.values().stream()
                 .map(MemoResponseDto::new).toList();
 
         return responseList;
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 db에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            // 메모 수정
+            memo.updapte(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 삭제하기
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다");
+        }
     }
 }
